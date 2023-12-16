0ec0f76afdb8202b8f33a80f1918559b10f1d19d
louisdies <luis.sotochaves@ucr.ac.cr>
2023-12-15 20:43:36 -0600

    Admin funcional

--- mysite/product_funcs/admin.py
+++ mysite/product_funcs/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Product
 
-#
-admin.site.register(Product)
+#Permite mostrar al admin y modificar los campos de la instancia en la base de datos.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'weight', 'base_cost_per_kilogram', 'image', 'is_active')
+    list_editable = ('weight', 'base_cost_per_kilogram', 'stock', 'image', 'is_active')
 
-# Register your models here.
+#Registra el modelo en la base de datos.
+admin.site.register(Product, ProductAdmin)
--- mysite/product_funcs/models.py
+++ mysite/product_funcs/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Modelo de los productos.
-class Product(models.model):
-    name = models.CharFiel(max_length=50)
+class Product(models.Model):
+    name = models.CharField(max_length=50)
     weight = models.DecimalField(max_digits=10, decimal_places=2)
     base_cost_per_kilogram = models.DecimalField(max_digits=10, decimal_places=2)
     price = models.DecimalField(max_digits=10, decimal_places=2)
