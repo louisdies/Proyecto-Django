53167e0a86851cee03fb03ba377142eb74493b12
louisdies <luis.sotochaves@ucr.ac.cr>
2023-12-18 14:10:38 -0600

    Añadido slug

--- mysite/product_funcs/admin.py
+++ mysite/product_funcs/admin.py
@@ -3,8 +3,8 @@ from .models import Product
 
 #Permite mostrar al admin y modificar los campos de la instancia en la base de datos.
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock', 'weight', 'base_cost_per_kilogram', 'image', 'is_active')
-    list_editable = ('weight', 'base_cost_per_kilogram', 'stock', 'image', 'is_active')
+    list_display = ('name', 'price', 'stock', 'weight', 'base_cost_per_kilogram', 'image', 'is_active', 'slug')	
+    list_editable = ('weight', 'base_cost_per_kilogram', 'stock', 'image', 'is_active', 'slug')
 
 #Registra el modelo en la base de datos.
 admin.site.register(Product, ProductAdmin)
--- mysite/product_funcs/models.py
+++ mysite/product_funcs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Modelo de los productos.
 class Product(models.Model):
@@ -9,8 +10,15 @@ class Product(models.Model):
     stock = models.IntegerField()
     image = models.ImageField(upload_to='products', null=True, blank=True)
     is_active = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True, blank=True)
     
     #Método predeterminado de Django para guardar cálculo en base de datos.
     def save(self, *args, **kwargs):
         self.price = self.weight * self.base_cost_per_kilogram
         super().save(*args, **kwargs)
+
+    #Genera slugs si el administrador no lo crea.
+    def save(self, *args, **kwargs):
+            if not self.slug:  # if slug wasn't provided, create one from the name
+                self.slug = slugify(self.name)
+            super(Product, self).save(*args, **kwargs)
